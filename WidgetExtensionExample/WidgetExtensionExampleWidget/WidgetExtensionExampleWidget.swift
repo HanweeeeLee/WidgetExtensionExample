dbdea9417676f23d7a796f5400fc2c9f47683146
Hanwe Lee <mobiledev@kakao.com>
2020-11-08 17:18:40 +0900

    no message

--- WidgetExtensionExample/WidgetExtensionExampleWidget/WidgetExtensionExampleWidget.swift
+++ WidgetExtensionExample/WidgetExtensionExampleWidget/WidgetExtensionExampleWidget.swift
@@ -11,15 +11,27 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+        print("[life cycle] placeholder")
+        return SimpleEntry(date: Date(), configuration: ConfigurationIntent())
     }
 
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) { // 위젯을 추가하는등의 경우에 스냅샷을 요구 할 수 있다.
+        print("[life cycle] getSnapshot")
         let entry = SimpleEntry(date: Date(), configuration: configuration)
+        
+        if context.isPreview {
+            print("이것은 미리보기입니다.")
+        }
+        else {
+            print("이것은 미리보기가 아닌 진짜 위젯입니다.")
+        }
+        
+        
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        print("[life cycle] getTimeline")
         var entries: [SimpleEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
@@ -30,25 +42,55 @@ struct Provider: IntentTimelineProvider {
             entries.append(entry)
         }
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let timeline = Timeline(entries: entries, policy: .never)
         completion(timeline)
     }
 }
 
 struct SimpleEntry: TimelineEntry {
-    let date: Date
+    let date: Date // required
     let configuration: ConfigurationIntent
+    let fromAppData: String = ""
 }
 
 struct WidgetExtensionExampleWidgetEntryView : View {
+    @Environment(\.widgetFamily) private var widgetFamily //이런식으로 분기 처리 할 수 있음
     var entry: Provider.Entry
 
     var body: some View {
         Text(entry.date, style: .time)
+        switch widgetFamily {// 분기
+        case .systemLarge:
+            Text("systemLarge")
+            largeView
+        case .systemMedium:
+            Text("systemMedium")
+            mediumView
+        case .systemSmall:
+            Text("systemSmall")
+        @unknown default:
+            Text("unkown")
+        }
+    }
+    
+    var largeView: some View {
+        Text("this is large hello world")
+    }
+    
+    var mediumView: some View {
+        Text("this is medium hello world")
     }
 }
 
-@main
+//struct MediumView: Struct {
+//    var body: some View {
+//        VStack {
+//
+//        }
+//    }
+//}
+
+
 struct WidgetExtensionExampleWidget: Widget {
     let kind: String = "WidgetExtensionExampleWidget"
 
@@ -58,6 +100,20 @@ struct WidgetExtensionExampleWidget: Widget {
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
+        .supportedFamilies([.systemMedium, .systemLarge]) // 지원하는 위젯 크기를 지정해 줄 수 있다.
+    }
+}
+
+@main
+struct WidgetExamplesWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetBundle1.Body()
+    }
+}
+
+struct WidgetBundle1: WidgetBundle {
+    var body: some Widget {
+        WidgetExtensionExampleWidget()
     }
 }
 
